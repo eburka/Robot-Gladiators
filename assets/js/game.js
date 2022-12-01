2a867c55a443f946a83dd6d019c1bf8501f7ca42
Elilta Burka <eliltaburka@yahoo.com>
2022-12-01 00:24:03 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 console.log(playerName, playerHealth, playerAttack)
 
@@ -14,6 +15,12 @@ var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
+    // Prompt the player if they want to fight or skip the battle.
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'Fight' or 'Skip' to choose.");
+
+    // If player choses to fight, then fight
+    if (promptFight ==="fight" || promptFight === "FIGHT") {
+   
     // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     enemyHealth = enemyHealth - playerAttack;
 
@@ -28,6 +35,27 @@ var fight = function() {
         window.alert(enemyName + " still has " + enemyHealth + " health left." );
     }
     
+    // If player choses to skip
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        
+        // Confirm player wants to skip 
+        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+        // If yes (true), leave fight
+        if (confirmSkip){
+            window.alert(playerName + " has decided to skip this fight. Goodbye!");
+            // Subtract money from playerMoney for skipping 
+            playerMoney = playerMoney - 2;
+        }
+        // If no (false), ask question again by running fight() again
+        else {
+            fight();
+        }
+
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
+
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the in the 'playerHealth' variable.
     playerHealth = playerHealth - enemyAttack;
 
